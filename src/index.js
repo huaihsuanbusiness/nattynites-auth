c1e1a31023420b54f23cb4d7210a66ed694f7fc2
huaihsuanbusiness <ilj9549@gmail.com>
2024-11-06 18:34:41 +0800

    update

--- src/index.js
+++ src/index.js
@@ -10,9 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("JavaScript is working!");
-});
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
